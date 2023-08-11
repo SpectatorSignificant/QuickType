6185057bcc9a7beac0f48192e0e407fa68c41adb
unknown <anishrajan2004@gmail.com>
2023-08-11 10:22:22 +0530

    Add settings button

--- app.js
+++ app.js
@@ -10,28 +10,27 @@ app.use(express.json());
 
 let randomWords;
 
+function randomizeArray(array) {
+    var swapIndex;
+    for (var i = 0; i < array.length; i++){
+        swapIndex = Math.floor(Math.random()*array.length);
+        [array[swapIndex], array[i]] = [array[i], array[swapIndex]];
+    }
+  
+    return array;
+}
+
 app.get("/", (req, res) => {
     res.redirect("/test");
 })
 
 app.get("/test", (req, res) => {
-    randomWords = generate({exactly: 3, minLength: 6, maxLength: 6}).concat(generate({exactly: 16, minLength: 2, maxLength: 5}));
+    randomWords = generate({exactly: 4, minLength: 6, maxLength: 6}).concat(generate({exactly: 16, minLength: 2, maxLength: 5}));
     randomWords = randomWords.concat(generate({exactly: 1, minLength: 7, maxLength: 8}));
-    console.log(randomWords);
-    function randomizeArray(array) {
-        var swapIndex;
-        for (var i = 0; i < array.length; i++){
-            swapIndex = Math.floor(Math.random()*array.length);
-            [array[swapIndex], array[i]] = [array[i], array[swapIndex]];
-        }
-      
-        return array;
-    }
     randomizeArray(randomWords);
-    console.log(randomWords);
     res.render("index.ejs", {randomWords})
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started on PORT 3000");
+app.listen(process.env.PORT || 3001, () => {
+    console.log("Server started on PORT 3001");
 })
--- public/script2.js
+++ public/script2.js
@@ -2,18 +2,23 @@ const wordsBox = document.querySelector(".words");
 const timerBox = document.querySelector(".time");
 const restartButton = document.querySelector("a.restart-button");
 const scoreBox = document.querySelector(".score");
+const settingsButton = document.querySelector("#btn-settings");
+const settingsCloseButton = document.querySelector("#btn-settings-close");
+const settingsOverlay = document.querySelector(".overlay")
 
 let spanObject, currentTime, elapsedTimeSeconds, lastCorrectUserIndex, buffer;
 let userString = "";
 let charArray = [];
 let originalIndex = 0;
 let index = 0;
+let score;
+let gameOver = false;
 let userIndex = 0;
 let wrongChars = 0;
 let currentWord = 0;
 let badEntry = false;
 let spaceWasPressed = false;
-let startTime = Date.now();
+let startTime;
 
 originalString = randomWords.replace(/,/g, " ");
 console.log('length of original string:', originalString.length);
@@ -29,12 +34,10 @@ function updateWordClass(index){
         charArray[i].classList.remove("current-word");
     }
 
-    for (var j = index - 1; j >= 0 && charArray[j].textContent != " "; j--) console.log(j);
-    console.log(j);
+    for (var j = index - 1; j >= 0 && charArray[j].textContent != " "; j--) continue;
 
     for (var k = j + 1; k < charArray.length && charArray[k].textContent != " "; k++){
         charArray[k].classList.add("current-word");
-        console.log(k);
     }
 }
 
@@ -62,14 +65,24 @@ for (var j = 0; j < originalString.length; j++){
 displayChars();
 updateWordClass(userIndex);
 
+settingsButton.onclick = (e) => {
+    settingsOverlay.hidden = false;
+}
+
+settingsCloseButton.onclick = (e) => {
+    settingsOverlay.hidden = true;
+    wordsBox.style["text-shadow"] = "none";
+    wordsBox.style.color = "black";
+    console.log(wordsBox.style)
+}
+
 document.addEventListener("keydown", (e) => {
-    console.log((currentTime - startTime)/1000);
     if (e.key.length == 1 || e.key == "Backspace"){
         if (userIndex == 1){
             startTime = Date.now();
         } 
         // if (userIndex > 1) console.log(charArray[userIndex - 1].matches(".correct"));
-        if (e.key == originalString[originalIndex].toLocaleLowerCase() && wrongChars == 0){
+        if (originalIndex < originalString.length && e.key == originalString[originalIndex].toLocaleLowerCase() && wrongChars == 0){
             if (e.key == " " && spaceWasPressed == false){
                 spaceWasPressed = true;
                 e.key = "_";
@@ -117,6 +130,12 @@ document.addEventListener("keydown", (e) => {
             spaceWasPressed = true;
             // console.log(buffer, originalIndex, userIndex, wrongChars);
         }
+        
+        if (originalIndex >= originalString.length) {
+            console.log("Over", { score })
+            scoreBox.innerText = score;
+            clearInterval(interval)
+        }
     }
     
     // console.log(e.key, originalIndex, userIndex, originalString[originalIndex]);
@@ -130,12 +149,20 @@ document.addEventListener("keydown", (e) => {
 //     scoreBox.innerText = Math.round(elapsedTimeSeconds);
 // })
 
+// let count = 0
+// const inter = setInterval(() => {
+//     console.log(count)
+//     count++
+//     if (count == 5) clearInterval(inter)
+// }, 500)
+
 const interval = setInterval(() => {
     currentTime = Date.now();
     elapsedTimeSeconds = (currentTime - startTime)/1000
-    timerBox.innerText = (elapsedTimeSeconds);
-    // if (userIndex >= charArray.length - 1){
-        scoreBox.innerText = (60*(originalString.length/5)/elapsedTimeSeconds);
-    // }
+    if (!isNaN(elapsedTimeSeconds)){
+        timerBox.innerText = (elapsedTimeSeconds)
+    }
+    score = 60*(originalString.length/5)/elapsedTimeSeconds
+    
 }, 50);
 
